e1e6c818ff7fa6dfb6717f6c388dc6b7b17dfb0a
Aleski <aleksey_minsk@yahoo.com>
2020-09-24 20:04:20 +0300

    Initial commit

---
+++ src/main/java/com/reboot/redemption/RedemptionApplication.java
@@ -0,0 +1,13 @@
+package com.reboot.redemption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedemptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedemptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reboot/redemption/RedemptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot.redemption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedemptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
